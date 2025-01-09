0a13317a2f753f38a0f4fe3ce0fd6e2639d02dbc
Piinfany <jirada.dec@gmail.com>
2025-01-09 14:25:06 +0700

    Update Control_Flow3.py

--- Control_Flow3.py
+++ Control_Flow3.py
@@ -4,7 +4,21 @@ Write a Python program that iterates through the numbers 1 to 100 and
 applies specific rules to determine what to print for each number. 
 This exercise will help you practice working with loops, conditionals, and basic arithmetic operators.
 '''
-for fizzbuzz in range(101):
+for fizzbuzz in range(1,101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("FizzBuzz")
+    elif fizzbuzz % 3 == 0:
+        print("Fizz")
+    elif fizzbuzz % 5 == 0:
+        print("Buzz")
+    else:
+        print(fizzbuzz)
+
+
+#Additional Challenge
+number1 = int(input("Enter your number1 : "))
+number2 = int(input("Enter your number2 : "))
+for fizzbuzz in range(number1,number2+1):
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
         print("FizzBuzz")
     elif fizzbuzz % 3 == 0:
