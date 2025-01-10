774f0bd0b9c090592f48c677905b97a695a34766
cchosungnoen <36058586+cchosungnoen@users.noreply.github.com>
2025-01-10 11:03:45 +0700

    Update Control_Flow1.py
    validate input จาก user ก่อนนำไปใช้งาน

--- Control_Flow1.py
+++ Control_Flow1.py
@@ -1,4 +1,5 @@
 #Write a Python program that calculates the factorial of a number using a loop.
+# Validate input ก่อนเอาค่าไปใช้งาน
 number = int(input("Enter your number : "))
 fac = 1
 for i in range(1,number+1): 
